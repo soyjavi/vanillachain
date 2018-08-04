c356934569d31b06bf4d4d9b00763fa277b4cafa
@soyjavi <javi.jimenez.villar@gmail.com>
2018-08-04 09:02:20 +0700

    Improve chain validator

--- src/Blockchain/isValidChain.js
+++ src/Blockchain/isValidChain.js
@@ -1,9 +1,12 @@
+import { calculateHash } from 'Block';
+
 export default (chain = []) => {
   for (let i = 1; i < chain.length; i += 1) {
     const currentBlock = chain[i];
     const previousBlock = chain[i - 1];
 
     if (currentBlock.previousHash !== previousBlock.hash) return false;
+    if (currentBlock.hash !== calculateHash(currentBlock)) return false;
   }
 
   return true;
