521c2199d0440f88612c98d61307bdc8b6f59c4e
@soyjavi <javi.jimenez.villar@gmail.com>
2018-08-08 21:29:08 +0700

    When ask for a new block in network, broadcast it to peers

--- src/network/block.js
+++ src/network/block.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
+
+import { C } from 'common';
 import { broadcast } from './modules';
 
 const router = Router();
+const {
+  SOCKET: { MESSAGE: { BLOCK_PREMINE } },
+} = C;
 
 // Endpoints
 router.post('/', (req, res) => {
-  broadcast({ block: { hello: 'naivechain' } });
-  res.json({ block: undefined });
+  broadcast({ type: BLOCK_PREMINE, data: req.body });
+  res.json({ block: BLOCK_PREMINE });
 });
 
 router.get('/last', (req, res) => {
